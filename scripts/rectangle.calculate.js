9c96eb6a339cfe9fdcd23fc0d1c28e78afcc0272
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-29 12:53:53 +0600

    commited

--- scripts/rectangle.calculate.js
+++ scripts/rectangle.calculate.js
@@ -1,4 +1,24 @@
 function rectangaleArea (){
     const rectangleLength = document.getElementById('rectangle-Length');
-    console.log(rectangleLength)
+    const lengthText = rectangleLength.value;
+    const length = parseFloat(lengthText)
+    console.log(lengthText);
+
+    //get width of the rectangle
+
+    const widthInput = document.getElementById('rectangale-width');
+    const widthText = widthInput.value;
+    const width = parseFloat(widthText);
+    console.log(width);
+
+
+    //calculate rectangle area 
+
+    const area = length * width;
+    console.log("area of rectangle", area);
+
+    //display rectangle area
+
+    const rectangleAreaSapn = document.getElementById('rectangle-span');
+    rectangleAreaSapn.innerText = area;
 }
