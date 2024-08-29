c630d0a91917cafa016e098700c1192eb306335a
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-29 16:16:32 +0600

    commited

---
+++ scripts/penthagon-area.js
@@ -0,0 +1,21 @@
+function calculatePentagonArea(){
+    // console.log('penthagon')
+
+    const perimeter = getInputValueById('pen');
+    const apothem = getInputValueById('apo');
+    const area = 0.5* perimeter * apothem;
+    setInnerText('pentagon-span', area)
+
+}
+
+function getInputValueById(inputFieldId){
+    const inputField = document.getElementById(inputFieldId);
+    const inputValueText = inputField.value;
+    const value = parseFloat(inputValueText);
+    return value;
+}
+
+function setInnerText (elementId, area ){
+    const element = document.getElementById(elementId);
+    element.innerText = area;
+}
