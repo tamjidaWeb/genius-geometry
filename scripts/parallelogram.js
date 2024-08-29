d0267158590eb6d64da2ca98c8328a2f14e7e530
tamjidaWeb <tamjidajyoti97@gmail.com>
2024-08-29 13:51:41 +0600

    commited

---
+++ scripts/parallelogram.js
@@ -0,0 +1,57 @@
+// function calculateParallelogramArea(){
+//     const baseInput = document.getElementById('parallelo-base');
+//     const baseText = baseInput.value;
+//     const base = parseFloat(baseText);
+//     console.log(base);
+
+
+//     //height
+
+//     const heightInput = document.getElementById('parallelogram-height');
+//     const heightText = heightInput.value;
+//     const height = parseFloat(heightText);
+//     console.log(height);
+
+
+//     const area = base * height;
+//     console.log('parallelogram area is', area);
+
+//     //
+//     const paralleloSpan = document.getElementById('parallelo-span');
+//     paralleloSpan.innerText = area;
+
+// const base = getParalellogramBase();
+// console.log(base);
+// }
+
+
+// function getParalellogramBase(){
+//     const baseInput = document.getElementById('parallelo-base');
+//         const baseText = baseInput.value;
+//         const base = parseFloat(baseText);
+//         return base;
+        
+// }
+
+function calculateParallelogramArea()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
